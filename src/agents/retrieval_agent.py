c177b0a576c37213adf9859688f6063c1008043f
DiegoViera1511 <diegovieramartinez@gmail.com>
2025-06-21 23:38:59 -0400

    Added fallback logic with crawler and scraper for RAG

--- src/agents/retrieval_agent.py
+++ src/agents/retrieval_agent.py
@@ -2,17 +2,29 @@
 from schemas.estado import EstadoConversacion
 from langchain_community.vectorstores import FAISS
 from src.utils.crawler import Crawler
+from utils.retrieval_utils import update_db
 
-THRESHOLD = 0.5
+THRESHOLD = 1.2
 
 def crear_agente_retrieval(db: FAISS):
     crawler = Crawler()
     def manejar_retrieval(estado: EstadoConversacion) -> EstadoConversacion:
         documentos = db.similarity_search_with_score(estado.tema, k=10)
-        filtered_docs = [doc.page_content for doc, score in documentos if score > THRESHOLD]
+        filtered_docs = [doc.page_content for doc, score in documentos if score < THRESHOLD]
         
         if len(filtered_docs) < 4:
-            raise ValueError(f"No se encontraron documentos relevantes para el tema: {estado.tema}")
+            print(f"[RETRIEVAL AGENT] Buscando nuevos documentos para: {estado.tema}")
+            crawl_response = crawler.crawl(estado.tema, num_results=10)
+            urls = [result.get("url", "") for result in crawl_response.data]
+            docs = [crawler.scrape(url) for url in urls if url]
+            docs_text= [doc.markdown[:1500] for doc in docs if doc]
+            update_db(db, docs_text)
+            documentos = db.similarity_search_with_score(estado.tema, k=10)
+            filtered_docs = [doc.page_content for doc, score in documentos if score < THRESHOLD]            
+            if len(filtered_docs) == 0:
+                print(f"[RETRIEVAL AGENT] No se encontraron suficientes documentos relevantes para: {estado.tema}")
+                estado.docs_relevantes = []
+                return estado
         
         estado.docs_relevantes = filtered_docs
 
