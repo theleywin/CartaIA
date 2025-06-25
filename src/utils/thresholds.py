57c4100b0eeefaa7e647491a05a84bfc6bb54b3f
CfM47 <jossuearteche000@gmail.com>
2025-06-25 12:13:47 -0400

    feat: 🎸 refactor threshol acceptance function

---
+++ src/utils/thresholds.py
@@ -0,0 +1,7 @@
+def is_relevant_cosine(score: float) -> bool:
+    threshold = 0.5
+    return score > threshold
+
+def is_relevant_l2(score: float) -> bool:
+    threshold = 1.2
+    return score < threshold
